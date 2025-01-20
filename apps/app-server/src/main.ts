a6e3e45b4bcec0e90b9c509d49641b0c68f01985
fruitsdrink <fruitsdrink@126.com>
2025-01-20 09:43:39 +0800

    更改系统结构为Monorepo

--- apps/app-server/src/main.ts
+++ apps/app-server/src/main.ts
@@ -3,6 +3,6 @@ import { AppServerModule } from './app-server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppServerModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
