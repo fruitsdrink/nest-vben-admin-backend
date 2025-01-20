bc4abaef34b59a136e948121f3a15d4ecade7ff3
fruitsdrink <fruitsdrink@126.com>
2025-01-20 12:01:26 +0800

    启动端口应用配置文件

--- apps/back-end-server/src/main.ts
+++ apps/back-end-server/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Configuration } from './types';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const port = configService.get('http.port');
+  const configService = app.get(ConfigService) as ConfigService<Configuration>;
+  const port = configService.get('http.port', { infer: true }); // 获取配置文件中的端口号
+
   console.log(port);
   await app.listen(port, () => {
     console.log(`Nest-Vben-Admin-Backend is running on: ${port}`);
