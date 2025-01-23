6acea134c667ee8c5ece79ddfca5ec23e4ff937e
fruitsdrink <fruitsdrink@126.com>
2025-01-23 15:18:34 +0800

    feat: 用户头像从后盾获取

--- apps/back-end-server/src/modules/user/user.service.ts
+++ apps/back-end-server/src/modules/user/user.service.ts
@@ -1,10 +1,15 @@
+import { Configuration } from '@/types';
 import { AuthUser } from '@/types/prisma';
 import { PrismaService } from '@lib/system/modules';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly config: ConfigService<Configuration>,
+  ) {}
 
   /**
    *
@@ -14,9 +19,15 @@ export class UserService {
     if (!user) {
       throw new UnauthorizedException('用户未登录');
     }
+    const host = this.config.get('http.host', { infer: true });
+    const avatar = user.avatar
+      ? `${host}/${user.avatar}`
+      : `${host}/public/images/avatar.webp`;
+
     return {
       id: Number(user.id),
       username: user.username,
+      avatar,
       roles: [],
     };
   }
